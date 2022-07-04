3013835f6144bdc3cac52a23676659354ea677c1
tccSolutions <tim@tccs.tech>
2022-07-04 13:12:24 -0400

    update to database cred

--- classes/Database.php
+++ classes/Database.php
@@ -8,11 +8,11 @@ class Database
     function connect()
     {
         $db_host = 'us-cdbr-east-06.cleardb.net';
-        $db_name = 'b0b4d2c45ab89d';
+        $db_name = 'heroku_ce8889f441fe159';
         $db_user = 'b0b4d2c45ab89d';
         $db_password = 'a550af6c';
         $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
-        try {
+        try {           
             return new PDO($dsn, $db_user, $db_password);
         } catch (Exception $e) {
             return $errors[0] = $e->getMessage();
@@ -21,3 +21,4 @@ class Database
     }
 
 }
+
