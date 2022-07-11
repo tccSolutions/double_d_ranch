047db022c25684eecd57576946a9bf1726057fd6
tccSolutions <83311084+tccSolutions@users.noreply.github.com>
2022-07-10 21:19:14 -0400

    Removed var dump

--- admin/images.php
+++ admin/images.php
@@ -9,10 +9,9 @@ $images = Image::get_horse_images($conn, $horse->id);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   try{
     $image = new Image();
-   var_dump($_FILES["horse_image"]);
+   
     $cloud_image = CloudImage::upload($_FILES["horse_image"]["tmp_name"], $horse);
-    var_dump($image);
-    var_dump($cloud_image);
+   
     $image->comment = $_POST['comment'];
     $image->public_id = $cloud_image["public_id"];
     $image->url = $cloud_image["secure_url"];
